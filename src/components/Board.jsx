bfdcda135b8fe0fa78f6155aaa18e4d58b5a44f7
Nguyen Trung Chien <145978758+tuci31203@users.noreply.github.com>
2024-02-22 13:39:23 +0700

    kanbanBoard reconstruct

---
+++ src/components/Board.jsx
@@ -0,0 +1,43 @@
+import { React, useState } from "react";
+import Column from "./Column";
+import BurnBarrel from "./BurnBarrel";
+
+const Board = (props) => {
+    const [cards, setCards] = useState(props.defaultCards);
+
+    return (
+        <div className="flex h-full w-full gap-3 overflow-scroll p-12">
+            <Column
+                title="Backlog"
+                column="backlog"
+                headingColor="text-neutral-500"
+                cards={cards}
+                setCards={setCards}
+            />
+            <Column
+                title="TODO"
+                column="todo"
+                headingColor="text-yellow-200"
+                cards={cards}
+                setCards={setCards}
+            />
+            <Column
+                title="In progress"
+                column="doing"
+                headingColor="text-blue-200"
+                cards={cards}
+                setCards={setCards}
+            />
+            <Column
+                title="Complete"
+                column="done"
+                headingColor="text-emerald-200"
+                cards={cards}
+                setCards={setCards}
+            />
+            <BurnBarrel setCards={setCards} />
+        </div>
+    );
+};
+
+export default Board;
